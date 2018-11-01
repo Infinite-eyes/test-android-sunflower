522bae7d43bf7d1b7800e308e51d807024895b5c
chenwm <cwm930215@gmail.com>
2018-11-01 19:26:41 +0800

    1.modify

--- app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.java
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.java
@@ -1,12 +1,17 @@
 package com.google.samples.apps.sunflower;
 
 import android.os.Bundle;
+import android.view.Gravity;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.ui.NavigationViewKt;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.samples.apps.sunflower.databinding.ActivityGardenBinding;
 /**
@@ -25,9 +30,26 @@ public class GardenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityGardenBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_garden);
         drawerLayout = binding.drawerLayout;
-//        NavController navController
 
-
-
+        NavController navController = Navigation.findNavController(this, R.id.garden_nav_fragment);
+        setSupportActionBar(binding.toolbar);
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout);
+        NavigationViewKt.setupWithNavController(binding.navigationView,navController);
     }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(drawerLayout,Navigation.findNavController(this,R.id.garden_nav_fragment));
+    }
+
+    public void onBackPressed(){
+        if(this.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenFragment.java
@@ -0,0 +1,28 @@
+package com.google.samples.apps.sunflower;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * @author chenweiming
+ * @version 1.0
+ * @mail cwm930215@gmail.com
+ * @since 2018/11/1 2:03 PM
+ **/
+public class GardenFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
